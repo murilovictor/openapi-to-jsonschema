e864181fec82ae94b5c51b4097f36e1ca11d9c05
Murilo Victor <murilovictor63@gmail.com>
2024-10-03 14:17:56 -0300

    disable ssl

--- src/services/CallServicePhaseOne.ts
+++ src/services/CallServicePhaseOne.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import * as https from "node:https";
 
 
 class CallServicePhaseOne {
 
     public async callService(url: string): Promise<string> {
         try {
-            const response = await axios.get(url);
+
+            const instance = axios.create({
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                })
+            });
+            const response = await instance.get(url);
             return response.data;
         } catch (error) {
             throw new Error(`Error call Service from ${url}: ${error}`);
